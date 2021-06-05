fd3ade0ff5ddf7923ccb05c5fd65d4383c0a2d48
Seth Root <sethroot.io@gmail.com>
2021-06-05 18:21:24 -0500

    refactoring Pickup to use Either

--- src/Action/Pickup.hs
+++ src/Action/Pickup.hs
@@ -2,10 +2,9 @@
 
 module Action.Pickup where
 
-import           Control.Error                  ( MaybeT(runMaybeT)
-                                                , fromMaybe
-                                                , headMay
-                                                , hoistMaybe
+import           Control.Error                  ( headMay
+                                                , hoistEither
+                                                , runExceptT
                                                 )
 import           Control.Lens                   ( (.=)
                                                 , Ixed(ix)
@@ -16,6 +15,7 @@ import           Control.Monad.IO.Class         ( MonadIO(..) )
 import           Control.Monad.State.Lazy       ( MonadIO(..)
                                                 , MonadState
                                                 )
+import           Control.Monad.Trans.Except     ( runExceptT )
 import           Control.Monad.Trans.Maybe      ( MaybeT(runMaybeT) )
 import           Data.Char                      ( toLower )
 import           Data.List                      ( elemIndex )
@@ -26,53 +26,52 @@ import           Types
 pickupAction :: (MonadState Game m, MonadIO m) => Maybe Input -> m ()
 pickupAction Nothing      = liftIO . putStrLn $ "What do you want to pickup?"
 pickupAction (Just input) = do
-    pickupResult <- pickup input
-    let msg = fromMaybe (dontSeeObject (input ^. raw)) pickupResult
-    liftIO . putStrLn $ msg
+    out <- pickup input
+    either printE printE out
+    where printE = liftIO . putStrLn
 
 dontSeeObject :: String -> String
 dontSeeObject object = "You don't see a " ++ object ++ " here."
 
-pickup :: (MonadState Game m) => Input -> m (Maybe String)
-pickup target = runMaybeT $ do
-    mItem <- parseItemM $ target ^. normal
-    item  <- hoistMaybe mItem
-    loc'  <- use loc
+pickup :: (MonadState Game m) => Input -> m (Either String String)
+pickup input = runExceptT $ do
+    loc'        <- use loc
+    containers' <- use containers
+    items'      <- use items
+    mItem       <- parseItemM $ input ^. normal
+    item        <- case mItem of
+        Nothing -> do
+            let out = dontSeeObject $ input ^. raw
+            hoistEither $ Left out
+        Just item -> hoistEither $ Right item
     if item ^. loc == ItemLoc loc'
         then do
             pickupItemMutation item
-            pure $ (item ^. name) ++ " taken."
-        else do
-            containers' <- use containers
-            let openContainersHere = filter
-                    (\c -> c ^. loc == Lobby && c ^. cState == Open)
-                    containers'
-            items' <- use items
-            let itemsInOpenContainers =
-                    [ (i, c)
-                    | i <- items'
-                    , c <- openContainersHere
-                    , i ^. loc == ItemContainer (c ^. uid)
-                    ]
-            let
-                found =
-                    headMay
-                        . filter
-                              (\(i, _) ->
-                                  fmap toLower (i ^. name) == target ^. normal
-                              )
-                        $ itemsInOpenContainers
-            case found of
-                Nothing                -> hoistMaybe Nothing
-                Just (item, container) -> do
-                    pickupItemMutation item
-                    pure
-                        $  "You take the "
-                        ++ (item ^. name)
-                        ++ " from the "
-                        ++ container
-                        ^. name
-                        ++ "."
+            let out = (item ^. name) ++ " taken."
+            hoistEither $ Left out
+        else hoistEither $ Right ()
+    let openContainersHere    = filter (containerOpenInLoc loc') containers'
+    let itemsInOpenContainers = itemsInContainers items' openContainersHere
+    let filtered              = filter (inputMatchesItem input) itemsInOpenContainers
+    case headMay filtered of
+        Nothing -> do
+            let out = "Something has gone terrible wrong"
+            hoistEither $ Left out
+        Just (item, container) -> do
+            pickupItemMutation item
+            let out = takeItemFromContainer item container
+            hoistEither $ Right out
+
+containerOpenInLoc :: Loc -> Container -> Bool
+containerOpenInLoc loc' container =
+    container ^. cState == Open && container ^. loc == loc'
+
+itemsInContainers :: [Item] -> [Container] -> [(Item, Container)]
+itemsInContainers is cs =
+    [ (i, c) | i <- is, c <- cs, i ^. loc == ItemContainer (c ^. uid) ]
+
+inputMatchesItem :: Input -> (Item, Container) -> Bool
+inputMatchesItem input (i, _) = fmap toLower (i ^. name) == input ^. normal
 
 pickupItemMutation :: MonadState Game m => Item -> m ()
 pickupItemMutation item = do
@@ -80,3 +79,9 @@ pickupItemMutation item = do
     case elemIndex item items' of
         Nothing -> pure ()
         Just i  -> items . ix i . loc .= ItemInv
+
+takeItemFromContainer :: Item -> Container -> String
+takeItemFromContainer item container =
+    let i = item ^. name
+        c = container ^. name
+    in  "You take the " ++ i ++ " from the " ++ c ++ "."
