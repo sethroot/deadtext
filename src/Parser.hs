dff4b6ea1bec4d3c012a4141bf9ec371d0fd61fa
Seth Root <sethroot.io@gmail.com>
2021-09-13 01:01:16 -0500

    cleaning up Parser

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@
 
 module Parser where
 
+import           Common                         ( inInventory )
 import           Control.Error                  ( MaybeT(runMaybeT)
                                                 , hoistMaybe
                                                 )
@@ -14,6 +15,7 @@ import           Data.Char                      ( toLower )
 import           Data.List                      ( find )
 import           Data.List.Split                ( splitOn )
 import           Types
+import           Util                           ( lowEq )
 
 parseRawInput :: String -> [String]
 parseRawInput = filter (/= "") . splitOn " "
@@ -26,62 +28,42 @@ normalizeInput = fmap $ fmap toLower
 parseInvObj :: MonadState Game m => String -> m (Maybe Obj)
 parseInvObj input = runMaybeT $ do
     items' <- use items
-    found  <- hoistMaybe $ find
-        (\x ->
-            x
-                ^. loc
-                == ItemInv
-                && (toLower <$> (x ^. name))
-                == (toLower <$> input)
-        )
-        items'
+    found  <- hoistMaybe $ find go items'
     pure $ ObjInv found
+    where go item = inInventory item && lowEq (item ^. name) input
 
 -- Attempt to parse input to an Item not held in Inventory
 
 parseItemObj :: MonadState Game m => String -> m (Maybe Obj)
 parseItemObj input = runMaybeT $ do
     items' <- use items
-    found  <- hoistMaybe $ find
-        (\x ->
-            x
-                ^. loc
-                /= ItemInv
-                && (toLower <$> (x ^. name))
-                == (toLower <$> input)
-        )
-        items'
+    found  <- hoistMaybe $ find go items'
     pure $ ObjItem found
+    where go item = not (inInventory item) && lowEq (item ^. name) input
 
 parseNpcObj :: MonadState Game m => String -> m (Maybe Obj)
 parseNpcObj input = runMaybeT $ do
     npcs' <- use npcs
-    found <- hoistMaybe $ find
-        (\x -> (toLower <$> (x ^. name)) == (toLower <$> input))
-        npcs'
+    found <- hoistMaybe $ find go npcs'
     pure $ ObjNpc found
+    where go npc = lowEq (npc ^. name) input
 
 parseItemM :: MonadState Game m => String -> m (Maybe Item)
 parseItemM input = do
     items' <- use items
-    pure $ find (\x -> (toLower <$> (x ^. name)) == (toLower <$> input)) items'
+    pure $ find go items'
+    where go item = lowEq (item ^. name) input
 
 parseInvItem :: MonadState Game m => String -> m (Maybe Item)
 parseInvItem input = do
     items' <- use items
-    pure $ find
-        (\x ->
-            x
-                ^. loc
-                == ItemInv
-                && (toLower <$> (x ^. name))
-                == (toLower <$> input)
-        )
-        items'
+    pure $ find go items'
+    where go item = inInventory item && lowEq (item ^. name) input
 
 parseNpc :: [Npc] -> String -> Maybe Npc
 parseNpc npcs input = do
-    find (\x -> (toLower <$> (x ^. name)) == (toLower <$> input)) npcs
+    find go npcs
+    where go npc = lowEq (npc ^. name) input
 
 parseNpcM :: MonadState Game m => String -> m (Maybe Npc)
 parseNpcM input = do
@@ -91,8 +73,8 @@ parseNpcM input = do
 parseContainer :: MonadState Game m => String -> m (Maybe Container)
 parseContainer input = do
     containers' <- use containers
-    pure $ find (\x -> (toLower <$> (x ^. name)) == (toLower <$> input))
-                containers'
+    pure $ find go containers'
+    where go cont = lowEq (cont ^. name) input
 
 parseContObj :: MonadState Game m => String -> m (Maybe Obj)
 parseContObj input = runMaybeT $ do
