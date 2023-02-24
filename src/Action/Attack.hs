e502432dc7e206c88a4498f70e1862291a8da6c1
Seth Root <sethroot.io@gmail.com>
2023-02-24 01:27:39 -0600

    minor attack cleanup

--- src/Action/Attack.hs
+++ src/Action/Attack.hs
@@ -6,8 +6,8 @@ import Control.Error (MaybeT(runMaybeT), hoistEither, hoistMaybe, runExceptT)
 import Control.Lens ((%~), Ixed(ix), (^.), use)
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.State.Lazy (MonadState(get, put))
-import Data.List (elemIndex)
-import Parser (parseRecM, parseNpcM)
+import Data.List (elemIndex, intersperse)
+import Parser (parseNpcM, parseRecM)
 import Safe (headMay)
 import Types
 
@@ -38,15 +38,19 @@ attack inputs = runExceptT $ do
             hoistEither . Left $ out
         Just dmg -> hoistEither $ Right dmg
 
-    avatar'   <- use avatar
-    avatar''' <- case avatar' of
+    avatar'  <- use avatar
+    avatar'' <- case avatar' of
         Nothing -> do
             let out = "Something has gone horribly wrong."
             hoistEither . Left $ out
-        Just avatar'' -> hoistEither $ Right avatar''
+        Just a -> hoistEither $ Right a
 
-    let combatOut = avatar''' ^. combat . description $ npc'
-    pure . mconcat $ [combatOut, "\n", npc' ^. name ++ " looks hurt."]
+    pure
+        . mconcat
+        . intersperse "\n"
+        $ [ avatar'' ^. combat . description $ npc'
+          , npc' ^. name ++ " looks hurt."
+          ]
 
 attackMutation :: MonadState Game m => Npc -> m (Maybe Int)
 attackMutation targetNpc = runMaybeT $ do
@@ -54,10 +58,10 @@ attackMutation targetNpc = runMaybeT $ do
     index <- case elemIndex targetNpc npcs' of
         Nothing -> hoistMaybe Nothing
         Just i  -> pure i
-    game     <- get
-    avatar'  <- use avatar
-    avatar'' <- hoistMaybe avatar'
-    let dmg = avatar'' ^. combat . damage
+    game    <- get
+    avatarM <- use avatar
+    avatar' <- hoistMaybe avatarM
+    let dmg = avatar' ^. combat . damage
     put $ npcs . ix index . health %~ subtract dmg $ game
     pure dmg
     -- get >>= \g -> put $ npcs . ix index . health %~ subtract 10 $ g
