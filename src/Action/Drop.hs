4dedfd57ab2f547e0ad7b6caa28c1f09998d9676
Seth Root <sethroot.io@gmail.com>
2023-08-27 02:32:09 -0500

    adding multi word support for dropping inventory items

--- src/Action/Drop.hs
+++ src/Action/Drop.hs
@@ -3,13 +3,12 @@
 module Action.Drop (dropAction) where
 
 import Common (indefArt, period)
-import Control.Error
-    ((??), MaybeT(runMaybeT), headMay, hoistMaybe, runExceptT)
+import Control.Error ((??), MaybeT(runMaybeT), headMay, hoistMaybe, runExceptT)
 import Control.Lens ((.=), Ixed(ix), (^.), use)
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.State.Lazy (MonadState)
 import Data.List (elemIndex)
-import Parser (parseInvItemM)
+import Parser (parseInvItemM, parseRecM)
 import Types
 
 dropAction :: (MonadState Game m, MonadIO m) => [Input] -> m ()
@@ -20,8 +19,8 @@ dropAction inputs = do
 
 dropItem :: MonadState Game m => [Input] -> m (Either String String)
 dropItem inputs = runExceptT $ do
+    itemM      <- parseRecM parseInvItemM inputs 
     target     <- headMay inputs ?? dropWhat
-    itemM      <- parseInvItemM $ target ^. normal
     targetItem <- itemM ?? dontHaveObject (target ^. normal)
     let itemName = targetItem ^. name
     result <- dropMutation targetItem
